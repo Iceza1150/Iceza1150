6df35cb96eb6a5112abbe11f39e0861260b0ae8e
Iceza1150 <87591247+Iceza1150@users.noreply.github.com>
2021-09-04 12:24:16 +0700

    Create update.php

---
+++ update.php
@@ -0,0 +1,23 @@
+{
+//เวอร์ชั่นไว้สำหรับอัพเดทหากมีการอัพเดทต้องเปลี่ยนตัวเลขให้เยอะกว่าเดิม
+"Version":"1.34",
+//ลิ้งค์สำหรับอัพเดทเซิร์ฟเวอร์
+"Url":"https://github.com/fram1025/fram1025/raw/master/update.zip" ,
+"Changelog":"
+พบไฟล์ใหม่ (04/09/2021)
+ICEZAVPN
+",
+ 
+ 
+ 
+ 
+ 
+ 
+//ไม่ต้องยุ่งด้านล่าง
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
