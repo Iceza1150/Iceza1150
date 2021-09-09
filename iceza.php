216edd8f1d5e96dbc236210276225a3b3b095dd4
Iceza1150 <87591247+Iceza1150@users.noreply.github.com>
2021-09-09 07:28:52 +0700

    Create iceza.php

---
+++ iceza.php
@@ -0,0 +1,27 @@
+{
+//เวอร์ชั่นไว้สำหรับอัพเดทหากมีการอัพเดทต้องเปลี่ยนตัวเลขให้เยอะกว่าเดิม
+"Version":"2.9",
+//ลิ้งค์สำหรับอัพเดทเซิร์ฟเวอร์
+"Url":"https://github.com/Iceza1150/Iceza1150/raw/main/ice.zip" ,
+"Changelog":"
+พบไฟล์ใหม่ (2020)
+รองรับ ซิม TRUE & DTAC
+DTAC โปร SHOPEE
+DTAC โปร HAPPYWORK
+TRUE โปร LINE
+ 
+",
+ 
+ 
+ 
+ 
+ 
+ 
+//ไม่ต้องยุ่งด้านล่าง
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
